bacfcf428dff48d4cf6d62d7b8245001551f0e21
AbelolDev <155784727+AbelolDev@users.noreply.github.com>
2024-08-07 17:11:09 -0400

    initial archives

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Configura 'public' como el directorio de archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'webs', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'webs', 'about.html'))
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
